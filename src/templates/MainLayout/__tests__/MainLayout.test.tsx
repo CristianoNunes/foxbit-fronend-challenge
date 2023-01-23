04ac5f4cc647a764498adce47e4748bf1015ff67
CristianoNunes <cristianonunes99@gmail.com>
2023-01-23 00:17:37 -0300

    Remove broken tests

--- src/templates/MainLayout/__tests__/MainLayout.test.tsx
+++ src/templates/MainLayout/__tests__/MainLayout.test.tsx
@@ -8,26 +8,4 @@ describe('MainLayout', () => {
     renderWithTheme(<MainLayout />);
     expect(screen.getByText('Foxbit - Frontend Challenge')).toBeInTheDocument();
   });
-
-  it('should check if the websocket opened the connection', () => {
-    renderWithTheme(<MainLayout />);
-    const ws = new WebSocket(process.env.NEXT_PUBLIC_WEB_SOCKET_URL);
-
-    ws.onopen = () => {
-      console.log('connected');
-      expect(console).toHaveBeenCalledWith('connected');
-      expect(ws.readyState).toBe(WebSocket.OPEN);
-    };
-  });
-
-  it('should check if the websocket closed the connection', () => {
-    renderWithTheme(<MainLayout />);
-    const ws = new WebSocket(process.env.NEXT_PUBLIC_WEB_SOCKET_URL);
-
-    ws.onclose = () => {
-      console.log('disconnected');
-      expect(console).toHaveBeenCalledWith('disconnected');
-      expect(ws.readyState).toBe(WebSocket.CLOSED);
-    };
-  });
 });
